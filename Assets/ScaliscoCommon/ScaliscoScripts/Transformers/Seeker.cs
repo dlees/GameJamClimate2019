7a87ce4cc88356ea5b3fab735cbcd5533d33708a
Omri Perez <omrip@amazon.com>
2019-05-11 10:31:30 -0700

    Implementing enemy movement physics

--- Assets/ScaliscoCommon/ScaliscoScripts/Transformers/Seeker.cs
+++ Assets/ScaliscoCommon/ScaliscoScripts/Transformers/Seeker.cs
@@ -13,9 +13,17 @@ public class Seeker : MonoBehaviour {
 	public UnityEvent unityEvent;
 	public FloatReference speed;
 
-	void Start() {
+    // Physics movement variables
+    public Rigidbody2D body;
+    public float minDistanceToTarget = 1f;
+    public float accel;
+    public float turnSpeed;
+    public float maxSpeed;
+
+    void Start() {
         if (objectToMove == null) {
             objectToMove = this.gameObject.transform;
+            body = GetComponent<Rigidbody2D>();
         }
 	}
 
@@ -30,14 +38,49 @@ public class Seeker : MonoBehaviour {
     }
 
     private Quaternion getNewRotation() {
-        Vector3 vectorToTarget = destination.transform.position - transform.position;
-        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
-        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        Quaternion q = Quaternion.AngleAxis(angleToTarget(), Vector3.forward);
         return Quaternion.Slerp(transform.rotation, q, 1.0f);
 
     }
 
-	public void changeDestination(GameObject newDestination) {
+    private float angleToTarget()
+    {
+        Vector3 vectorToTarget = destination.transform.position - transform.position;
+        return Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
+    }
+
+    public void changeDestination(GameObject newDestination) {
 		destination = newDestination;
 	}
+
+    void MoveTowards(float moveSpeed)
+    {
+
+        //if up pressed
+        if (DistanceToTarget() > minDistanceToTarget)
+        {
+            //add force
+            body.AddRelativeForce(Vector2.up * accel);
+
+            //if we are going too fast, cap speed
+            if (body.velocity.magnitude > moveSpeed)
+            {
+                body.velocity = body.velocity.normalized * moveSpeed;
+            }
+        }
+        float newRotation = angleToTarget();
+        //if right/left pressed add torque to turn
+        if (Mathf.Abs(newRotation) > 0.001f)
+        {
+            //scale the amount you can turn based on current velocity so slower turning below max speed
+            float scale = Mathf.Lerp(0f, turnSpeed, body.velocity.magnitude / maxSpeed);
+            //axis is opposite what we want by default
+            body.AddTorque(-newRotation * scale);
+        }
+    }
+
+    float DistanceToTarget()
+    {
+        return Vector3.Distance(destination.transform.position, transform.position);
+    }
 }
