34224e030c5e359c764768f70ab80c98975d1394
dlees <dlees@umich.edu>
2019-05-11 11:26:38 -0700

    Board Notification shows up when close

---
+++ Assets/Scripts/PlayerApproachedEnemyCondition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerApproachedEnemyCondition : Condition
+{
+    private bool isApproached = false;
+    private GameObject enemyApproached = null;
+
+    public override bool isConditionSatisfied() {
+        return isApproached;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        enemyApproached = collision.gameObject;
+        isApproached = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision) {
+        isApproached = false;
+        enemyApproached = null;
+    }
+
+}
