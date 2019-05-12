bdef747e2dcf6251a1eca10f1291639d2bcd2a8d
dlees <dlees@umich.edu>
2019-05-11 21:19:02 -0700

    Added more turrets to enemy ship

--- Assets/Scripts/EnemyShipController.cs
+++ Assets/Scripts/EnemyShipController.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class EnemyShipController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool runAway;
+    public int numWaterJets;
+    public bool towardOrAwayPlayer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+
     }
 }
