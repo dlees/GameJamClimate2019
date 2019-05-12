d90e08d14fd0c83f3d819e899ff05648b3fa033c
dlees <dlees@umich.edu>
2019-05-12 14:14:56 -0700

    Main menu canvas separate

---
+++ Assets/Scripts/TempRateCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempRateCalculator : MonoBehaviour
+{
+    public FloatReference temp;
+    public FloatReference tempRateResult;
+    public FloatReference oilPrice;
+
+    public float initialIncreasePerMin = .5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempRateResult.Value = initialIncreasePerMin / 60 * oilPrice.Value / 980;
+        temp.Value += tempRateResult.Value * Time.deltaTime;
+    }
+}
