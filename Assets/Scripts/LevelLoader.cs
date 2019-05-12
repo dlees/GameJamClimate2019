78e0c718d7aa277906d64be96846322b6c6b94b1
Omri Perez <omrip@amazon.com>
2019-05-12 14:10:44 -0700

    added main menu

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
