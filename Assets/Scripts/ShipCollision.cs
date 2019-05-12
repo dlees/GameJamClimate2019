aaae9b16e5d0ef572374f90c76141f2753a49577
Omri Perez <omrip@amazon.com>
2019-05-11 20:33:24 -0700

    Added collision with tanker

---
+++ Assets/Scripts/ShipCollision.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCollision : MonoBehaviour
+{
+    public FloatReference healthVariableToDecrement;
+    public float collisionSpeedLower;
+    public float collisionSpeedHigher;
+
+    Rigidbody2D body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            float relativeSpeed = Vector2.SqrMagnitude(body.velocity -
+                other.GetComponent<Rigidbody2D>().velocity);
+            Debug.Log("Colided with other at speed: " + relativeSpeed);
+            if (relativeSpeed > collisionSpeedLower) {
+                healthVariableToDecrement.Value = 0;
+                // TODO: add small ship explosion
+            }
+            if (relativeSpeed > collisionSpeedHigher)
+            {
+                // TODO: add other ship explosion
+            }
+        }
+    }
+}
