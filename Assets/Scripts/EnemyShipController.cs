c36246e2ff78a6f996fd4fd7579bd2c488991358
dlees <dlees@umich.edu>
2019-05-11 21:56:32 -0700

    Enemy Ship Controller sets properties on the ship

--- Assets/Scripts/EnemyShipController.cs
+++ Assets/Scripts/EnemyShipController.cs
@@ -4,11 +4,49 @@ using UnityEngine;
 
 public class EnemyShipController : MonoBehaviour
 {
-    public bool runAway;
+    public string movementType;
     public int numWaterJets;
-    public bool towardOrAwayPlayer;
+
+    public Seeker movementSeeker;
+    public GameObject[] players;
+
+    public GameObject[] turrets;
+    public Defend2D[] defenders;
+
+    public TransformListReference cameraObjects;
 
     void Start() {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        switch (movementType) {
+            case "stopped":
+                movementSeeker.enabled = false;
+                break; 
 
+            case "away":
+                movementSeeker.destination = players[0];
+                movementSeeker.shouldMoveAway = true;
+                break;
+
+            case "toward":
+                movementSeeker.destination = players[0];
+                movementSeeker.shouldMoveAway = false;
+
+                break;
+
+            default: 
+                movementSeeker.destination = players[0];
+                movementSeeker.shouldMoveAway = true;
+                break;
+        }
+
+        for (int i = 0; i < turrets.Length; i++) {
+            turrets[i].SetActive(i < numWaterJets);
+        }
+
+        foreach (Defend2D defender in defenders) {
+            defender.target = players[0];
+        }
+
+        
     }
 }
