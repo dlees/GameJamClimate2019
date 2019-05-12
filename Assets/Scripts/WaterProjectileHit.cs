e5f6b8dff7379040428198ac43ed3f089918e019
Omri Perez <omrip@amazon.com>
2019-05-11 18:42:02 -0700

    Fixed hurt rate

--- Assets/Scripts/WaterProjectileHit.cs
+++ Assets/Scripts/WaterProjectileHit.cs
@@ -6,12 +6,20 @@ public class WaterProjectileHit : MonoBehaviour {
 
     public FloatReference healthVariableToDecrement;
     public float probabilityToBeHurt = 0.05f;
+    public float delayBetweenHits = 0.5f;
+    public float timeBetweenProjectiles = 0.05f;
+    public float timeFromLastHit = 0f;
     public float amountToLose;
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.tag == "WaterProjectile") {
+    private void Update()
+    {
+        timeFromLastHit += Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D other) { 
+        if (other.CompareTag("WaterProjectile") && timeFromLastHit > timeBetweenProjectiles) {
             if (Random.Range(0f,1f) < probabilityToBeHurt)
                 healthVariableToDecrement.Value -= amountToLose;
+            timeFromLastHit = 0;
         }
     }
 }
