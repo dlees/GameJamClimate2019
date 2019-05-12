39422e69c50f0fc9d6b9c9e5d377d7c972711265
dlees <dlees@umich.edu>
2019-05-12 15:52:42 -0700

    Added color of bar based on temp

---
+++ Assets/Scripts/ColorBasedOnTemp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ColorBasedOnTemp : MonoBehaviour
+{
+    public FloatReference temp;
+    public Image foregroundBar;
+    public Color[] colors;
+
+    void Update()
+    {
+        if (temp < .3) {
+            foregroundBar.color = colors[0]; 
+        } else
+        if (temp < 1) {
+            foregroundBar.color = colors[1];
+        } else
+        if (temp < 1.5) {
+            foregroundBar.color = colors[2];
+        } else
+        if (temp < 2) {
+            foregroundBar.color = colors[3];
+        } else
+        if (temp < 3.5) {
+            foregroundBar.color = colors[4];
+        }
+    }
+}
