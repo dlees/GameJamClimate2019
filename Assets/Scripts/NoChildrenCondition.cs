d098b5faf2cb0dfd38239b75ca0c3163b2eabc28
dlees <dlees@umich.edu>
2019-05-12 16:47:33 -0700

    Victory Screen!

---
+++ Assets/Scripts/NoChildrenCondition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoChildrenCondition : Condition
+{
+
+    public Transform parent;
+
+    public override bool isConditionSatisfied() {
+        return parent.childCount == 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = this.gameObject.transform;   
+    }
+
+
+}
