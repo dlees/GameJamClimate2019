1b8fcdd7d6819ccd686c267289a4230be4882cad
Omri Perez <omrip@amazon.com>
2019-05-12 12:43:24 -0700

    Added water animation

---
+++ Assets/Scripts/AnimateWater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class AnimateWater : MonoBehaviour
+{
+    public float speed=1;
+    public Tilemap tileMap;
+    Vector3 defaultAnchor = new Vector3(0.5f, 0.5f, 0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tileMap.tileAnchor = tileMap.tileAnchor + new Vector3(-speed*Time.deltaTime, -speed*Time.deltaTime, 0);
+        if (tileMap.tileAnchor.x < -0.5)
+            tileMap.tileAnchor = defaultAnchor;
+    }
+}
+
