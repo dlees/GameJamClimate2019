fb23be8b2d15894a22b54785b4735c57aacff799
dlees <dlees@umich.edu>
2019-05-12 12:44:49 -0700

    Edited Wakes to make them rotate and look diff for enemies!

---
+++ Assets/ScaliscoCommon/ScaliscoScripts/TransformReferences/QuaternionFromTransformBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class QuaternionFromTransformBehaviour : MonoBehaviour, Valueable<Quaternion> {
+    public Transform tranform;
+
+    public Quaternion Value {
+        get {
+            return tranform.rotation;
+        }
+
+        set {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
--- Assets/ScaliscoCommon/ScaliscoScripts/TransformReferences/QuaternionReference.cs
+++ Assets/ScaliscoCommon/ScaliscoScripts/TransformReferences/QuaternionReference.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class QuaternionReference : GenericReference<Quaternion, QuaternionSO, QuaternionProvider> {
+public class QuaternionReference : GenericReference<Quaternion, QuaternionSO, QuaternionFromTransformBehaviour> {
     public QuaternionReference(Quaternion value) : base(value) { }
 }
 
@@ -18,19 +18,4 @@ public class QuaternionSO : ScriptableObject, Valueable<Quaternion> {
             Quaternion = value;
         }
     }
-}
-
-public class QuaternionProvider : MonoBehaviour, Valueable<Quaternion> {
-    public Quaternion vector;
-
-    public Quaternion Value {
-        get {
-            return vector;
-        }
-
-        set {
-            throw new System.NotImplementedException();
-        }
-    }
-
 }
