fcae75ba1813360a5653f08706967341ea7a2dbb
dlees <dlees@umich.edu>
2019-05-17 15:53:39 -0700

    Created board suggestion

--- Assets/ScaliscoCommon/ScaliscoScripts/Transformers/AlphaChanger.cs
+++ Assets/ScaliscoCommon/ScaliscoScripts/Transformers/AlphaChanger.cs
@@ -12,10 +12,19 @@ public class AlphaChanger : MonoBehaviour
     public float seconds = 1;
 
     private float curTime = 0;
-    // Update is called once per frame
+
+    public bool pingPong = false;
+
     void Update()
     {
         curTime += Time.deltaTime;
-        float newAlpha = Mathf.Lerp(startAlpha, endAlpha, curTime/seconds);
-        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, newAlpha);  }
+        float newAlpha;
+        if (!pingPong) {
+            newAlpha = Mathf.Lerp(startAlpha, endAlpha, curTime / seconds);
+        } else {
+            newAlpha = Mathf.PingPong(curTime / seconds, endAlpha - startAlpha) + startAlpha;
+        }
+
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, newAlpha);
+    }
 }
